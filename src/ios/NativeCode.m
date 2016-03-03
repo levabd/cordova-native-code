53868c8b9c86afa48a2909be7e59fb66bf952106
Lucas Nova <pogorelowMax@gmail.com>
2016-03-03 19:56:51 +0600

    ios Test plugin

---
+++ src/ios/NativeCode.m
@@ -0,0 +1,29 @@
+#include "NativeCode.h"
+
+@implementation NativeCode
+
+- (void)pluginInitialize
+{
+}
+
+- (void)consoleLog:(CDVInvokedUrlCommand *)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSArray *args = command.arguments;
+        NSString *message = [args objectAtIndex:0];
+
+        CDVPluginResult *pluginResult;
+        if (message) {
+
+            NSLog(@"Value of string is %@", message);
+
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid application id: null was found"];
+        }
+
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+@end
