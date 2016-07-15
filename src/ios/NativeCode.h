2dff75665244caf903d583188c20dc626345cb3a
Lucas Nova <pogorelowMax@gmail.com>
2016-07-15 15:09:15 +0600

    iOS permissions

--- src/ios/NativeCode.h
+++ src/ios/NativeCode.h
@@ -1,8 +1,9 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface NativeCode : CDVPlugin
-
-- (void)open:(CDVInvokedUrlCommand *)command;
-
+- (void)iOsPermission:(CDVInvokedUrlCommand *)command;
+- (void)iosHasPermission:(CDVInvokedUrlCommand *)command;
 @end
--- src/ios/NativeCode.m
+++ src/ios/NativeCode.m
@@ -1,9 +1,11 @@
 #include "NativeCode.h"
 
+
 @implementation NativeCode
 
 - (void)pluginInitialize
 {
+
 }
 
 - (void)consoleLog:(CDVInvokedUrlCommand *)command
@@ -23,6 +25,166 @@
         }
 
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+-(void)iOsHasPermission:(CDVInvokedUrlCommand *)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSArray *args = command.arguments;
+        NSString *permissionName = [args objectAtIndex:0];
+
+
+
+        if ([permissionName isEqual:@"Camera"]) {
+            if ([AVCaptureDevice respondsToSelector:@selector(requestAccessForMediaType: completionHandler:)]) {
+
+                AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+
+                if(status == AVAuthorizationStatusAuthorized) { // authorized
+                    CDVPluginResult *pluginResult;
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                }
+                else if(status == AVAuthorizationStatusDenied){ // denied
+                    CDVPluginResult *pluginResult;
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"denied"];
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+                }
+                else if(status == AVAuthorizationStatusRestricted){ // restricted
+                    CDVPluginResult *pluginResult;
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"restricted"];
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+
+                }
+                else if(status == AVAuthorizationStatusNotDetermined){ // not determined
+                    CDVPluginResult *pluginResult;
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"not determined"];
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+                }
+
+
+            }else{
+                CDVPluginResult *pluginResult;
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }
+
+            NSLog(@"Try to has perm %@", permissionName);
+
+
+
+        } else if ([permissionName isEqual:@"Geolocation"]){
+
+            if([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedAlways){
+                CDVPluginResult *pluginResult;
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }
+            else if([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse){
+                CDVPluginResult *pluginResult;
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }
+            else if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined)
+            {
+
+                CDVPluginResult *pluginResult;
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"not determined"];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+            }
+            else if([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied)
+            {
+
+                CDVPluginResult *pluginResult;
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"denied"];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                //Location Services is off from settings
+
+            }
+            else if([CLLocationManager authorizationStatus] == kCLAuthorizationStatusRestricted)
+            {
+                CDVPluginResult *pluginResult;
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"restricted"];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }
+
+            NSLog(@"Try to has perm %@", permissionName);
+
+        }else {
+            CDVPluginResult *pluginResult;
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid application id: null was found"];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+
+
+    }];
+
+}
+
+-(void)iOsPermission:(CDVInvokedUrlCommand *)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSArray *args = command.arguments;
+        NSString *permissionName = [args objectAtIndex:0];
+
+
+
+        if ([permissionName isEqual:@"Camera"]) {
+
+
+            NSLog(@"Try to take perm %@", permissionName);
+
+            if ([AVCaptureDevice respondsToSelector:@selector(requestAccessForMediaType: completionHandler:)]) {
+                [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+
+                    // Will get here on both iOS 7 & 8 even though camera permissions weren't required
+                    // until iOS 8. So for iOS 7 permission will always be granted.
+                    if (granted) {
+                        // Permission has been granted. Use dispatch_async for any UI updating
+                        // code because this block may be executed in a thread.
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            NSLog(@"GRANTED!!");
+                            CDVPluginResult *pluginResult;
+                            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                        });
+
+
+                    } else {
+                        CDVPluginResult *pluginResult;
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Access Denied"];
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                        NSLog(@"DENIED!!");
+                        // Permission has been denied.
+                    }
+                }];
+            } else {
+                // We are on iOS <= 6. Just do what we need to do.
+                CDVPluginResult *pluginResult;
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }
+
+        } else if ([permissionName isEqual:@"Geolocation"]){
+
+            CDVPluginResult *pluginResult;
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+            NSLog(@"Try to take perm %@", permissionName);
+
+        }else {
+            CDVPluginResult *pluginResult;
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid application id: null was found"];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+
+
     }];
 }
 
